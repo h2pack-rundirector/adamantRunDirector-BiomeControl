f8943db3dd2a9cab29d5c3c8f925be264ce10d1e
maybe-adamant <maybe.adamant@gmail.com>
2026-06-07 21:58:11 -0400

    test: update owner id assertion

--- tests/TestEntrypoint.lua
+++ tests/TestEntrypoint.lua
@@ -87,7 +87,7 @@ function TestEntrypoint:testMainLuaBootsRealModule()
     })
 
     lu.assertNotNil(boot.liveModule)
-    lu.assertEquals(boot.liveModule.getHostId(), "adamantRunDirector-BiomeControl")
+    lu.assertEquals(boot.liveModule.getOwnerId(), "adamantRunDirector-BiomeControl")
     lu.assertEquals(boot.liveModule.getModuleId(), "BiomeControl")
     lu.assertEquals(boot.liveModule.getPackId(), "run-director")
     lu.assertEquals(#boot.callbacks.imgui, 1)
